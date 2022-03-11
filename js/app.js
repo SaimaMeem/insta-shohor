25002169c39442eeb1e7fa22cb7ca7921d3b330b
SaimaMeem <saimasultana1320@gmail.com>
2022-03-11 17:29:18 +0600

    report button issue resolved

--- js/app.js
+++ js/app.js
@@ -40,13 +40,11 @@ const switchTab = (id) => {
     document.getElementById("liked").style.display = "block";
     document.getElementById("posts").style.display = "none";
     document.getElementById("reported").style.display = "none";
-
     displayLikedPosts();
   } else {
     document.getElementById("reported").style.display = "block";
     document.getElementById("posts").style.display = "none";
     document.getElementById("liked").style.display = "none";
-
     displayReportedPosts();
   }
 };
@@ -153,7 +151,7 @@ const displayLikedPosts = () => {
 
 const displayReportedPosts = () => {
   const reportedPosts = getReportedPosts();
-  posts.forEach((post) => {
+  reportedPosts.forEach((post) => {
     const div = createPost(post);
     document.getElementById("reported").appendChild(div);
   });
