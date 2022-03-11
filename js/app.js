2580f10a491e7cadba4d7ca5f35c41a4b2da8959
SaimaMeem <saimasultana1320@gmail.com>
2022-03-11 17:17:46 +0600

    comment and post description issue resolved

--- js/app.js
+++ js/app.js
@@ -28,7 +28,7 @@ const reportPost = (id) => {
 };
 
 const displayContent = (text) => {
-  return text.length < 30 ? 'text' : text.slice(0, 30) + "<span class='fw-bold'>... read more</span>";
+  return text.length < 30 ? text : text.slice(0, 30) + "<span class='fw-bold'>...read more</span>";
 };
 
 const switchTab = (id) => {
@@ -53,7 +53,7 @@ const switchTab = (id) => {
 
 const createPost = (post) => {
   // const image = post.image;
-  // console.log(post);
+  console.log(post);
   const div = document.createElement("article");
   div.classList.add("post");
   div.innerHTML = `
@@ -121,9 +121,9 @@ const createPost = (post) => {
                   <div class="post__description">
                     <small>
                       <a class="post__name--underline" href="#">
-                          ${post.comments?.user}
+                          ${post.comments[0]?.user}
                       </a>
-                      ${post.comments?.text}
+                      ${post.comments[0]?.text}
                     </small>
                   </div>
                   <span class="post__date-time">30 minutes ago</span>
